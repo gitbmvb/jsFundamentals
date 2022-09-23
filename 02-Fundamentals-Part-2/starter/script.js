42aecd084edd1a45a52af6a5ea793772228f943b
gitbmvb <bruno.martval@gmail.com>
2022-09-23 16:56:36 -0300

    Working with functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -15,7 +15,7 @@ if(passTest)
 if(hasDriversLicense) console.log("I can drive");
 
 //const interface = 'Audio';
-*/
+
 function logger(name){
     console.log(`My name is ${name}!`);
 }
@@ -34,3 +34,61 @@ console.log(appleJuice);
 
 const test = logger("");
 console.log(test);
+
+
+
+// Functions Declarations x Expressions
+
+// Function declarations use the "function" keyword
+// Variables can assign a function value before its declaration
+const myBirdsAge = calcAge1(2018);
+console.log(myBirdsAge);
+
+function calcAge1(birthYear){
+    return 2022 - birthYear;
+}
+
+console.log(calcAge1(2003));
+
+//In a function expression, a variable becomes a function
+// (anonymous function)
+
+const calcAge2 = function (birthYear){
+    return 2022 - birthYear;
+}
+
+console.log(calcAge2(2003));
+
+
+
+// Arrow functions
+
+// Arrow functions is a shorter and fast-to-write type of function expression
+// If a arrow functions has multiple lines, it
+// must have a return
+
+
+const calcAge3 = birthYear => 2022 - birthYear;
+const yearRet = birthYear => 65 - calcAge3(birthYear);
+const yearNow = (birthYear, age) => birthYear + age;
+
+console.log(yearRet(2003));
+console.log(yearNow(2003, 19));
+
+*/
+
+// Functions calling other functions
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${cutFruitPieces(apples)} apple pieces and ${cutFruitPieces(oranges)} orange pieces.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+
